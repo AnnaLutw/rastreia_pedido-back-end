e652ace41c644c2089bf01dc0a4e56760b16ff7f
AnnaLu <correioaninha@gmail.com>
2025-02-17 12:49:52 -0300

    autoriza  back-end

--- server.js
+++ server.js
@@ -9,11 +9,13 @@ const app = express();
 const port = process.env.PORT || 3000
 
 // Middleware para permitir CORS
-app.use(cors({
-  origin: '*',
-  methods: 'GET',
-  allowedHeaders: 'Content-Type,Authorization'
-}));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); 
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 
 // Middleware para permitir JSON no corpo da requisição
 app.use(express.json());
