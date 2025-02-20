7d00a7873e8b2594baf80bc207d5f34557e3fd41
AnnaLu <correioaninha@gmail.com>
2025-02-20 08:30:18 -0300

    cors

--- server.js
+++ server.js
@@ -8,9 +8,13 @@ require('dotenv').config(); // Carrega variáveis do .env
 const app = express();
 const port = process.env.PORT || 3000
 
-// Middleware para permitir CORS
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); 
+  const allowedOrigin = "https://fidcomex.up.railway.app/rastreio";
+  
+  if (req.headers.origin === allowedOrigin) {
+    res.header("Access-Control-Allow-Origin", allowedOrigin);
+  }
+  
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
