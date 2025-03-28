f1539f2b3df4f49e83f132b4e1b4514572bd8b1c
AnnaLu <correioaninha@gmail.com>
2025-03-28 14:41:41 -0300

    refatora

--- webhook/webhook.js
+++ webhook/webhook.js
@@ -52,115 +52,97 @@ const formatCpfCnpj = (value) => {
         : cleanedValue.replace(/(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/, '$1.$2.$3/$4-$5');
 };
 
-// Valida CPF/CNPJ no banco
-const valida = async (cpf_cnpj, sequelize) => {
-    const formattedCpfCnpj = formatCpfCnpj(cpf_cnpj);
+const pesquisasSql = async(pesquisa, tipo, sequelize) => {
+    let filtro = '';
+    let replacements = { pesquisa };
 
-    if (!isValidCpfCnpj(cpf_cnpj)) {
-        return 'cpf_invalido' ;
+    if (tipo === 'cpf_cnpj') {
+        pesquisa = formatCpfCnpj(pesquisa);
+        if (!isValidCpfCnpj(pesquisa)) return 'cpf_invalido';
+
+        filtro = `AND (c.cpf = :pesquisa OR c.cnpj = :pesquisa OR c.marketplace_pedido = :pesquisa)`;
     }
 
+    if (tipo === 'pedido')  filtro = `AND ns.marketplace_pedido = :pesquisa`;
+    
+    if (tipo === 'email')  filtro = `AND c.email = :pesquisa`;
+    
     const result = await sequelize.query(
         `SELECT ns.chavenfe,
-                ns.marketplace_pedido,
+                ns.marketplace_pedido as pedido,
                 c.email,
                 c.razsocial, 
+                ns.intelipost_order,
                 CASE 
                     WHEN ns.parceiro = 'FIDCOMERCIOEXTERIOREIRELI' THEN 'Mercado Livre' 
                     WHEN ns.parceiro LIKE '%WAPSTORE%' THEN 'Site Fid ComeX' 
                     ELSE ns.parceiro 
-	            END AS "Portal"
+                END AS portal
         FROM nota_saida ns
         JOIN cliente c ON c.id_cliente = ns.id_cliente
-        WHERE c.cpf = :cpf_cnpj OR c.cnpj = :cpf_cnpj 
-        and ns.chavenfe <> ''`,
+        WHERE ns.chavenfe <> ''
+        ${filtro}`,
         {
-            type: Sequelize.QueryTypes.SELECT,
-            replacements: { cpf_cnpj: formattedCpfCnpj }
+            type: sequelize.QueryTypes.SELECT,
+            replacements: replacements
         }
     );
 
     return result;
-};
+}
 
 
 const validaCpfParaTroca = async (cpf_cnpj, sequelize, contactId) => {
-    const result = await valida(cpf_cnpj, sequelize);
+    const result = await pesquisasSql(cpf_cnpj, 'cpf_cnpj', sequelize);
 
-    if (result === "cpf_invalido") {
-        return { flag: "cpf_invalido", message: "CPF/CNPJ inválido" };
-    }
+    if (result === "cpf_invalido") return { flag: "cpf_invalido", message: "CPF/CNPJ inválido" };
+    
+    if (!result.length) return { flag: 'registro_nao_encontrado_troca', message: 'Nenhum registro encontrado' };
+    
 
-    if (!result.length) {
-        return { flag: 'registro_nao_encontrado_troca', message: 'Nenhum registro encontrado' };
-    }
-
-    return await validaParaCancelamentoTroca(result, contactId);
+    return await processaValidacaoTroca(result, contactId);
 };
 
 
 const validaPedidoParaTroca = async (pedido, sequelize, contactId) => {
-    const result = await sequelize.query(
-        `SELECT ns.chavenfe,
-                ns.marketplace_pedido,
-                c.email,
-                c.razsocial, 
-                CASE 
-                    WHEN ns.parceiro = 'FIDCOMERCIOEXTERIOREIRELI' THEN 'Mercado Livre' 
-                    WHEN ns.parceiro LIKE '%WAPSTORE%' THEN 'Site Fid ComeX' 
-                    ELSE ns.parceiro 
-	            END AS "Portal"
-        FROM nota_saida ns
-        JOIN cliente c ON c.id_cliente = ns.id_cliente
-        WHERE ns.marketplace_pedido = :pedido
-        and ns.chavenfe <> ''`,
-        {
-            type: Sequelize.QueryTypes.SELECT,
-            replacements: { pedido }
-        }
-    );
-    
+
+    const result=  await pesquisasSql(pedido, 'pedido', sequelize);
+
     if (!result.length) {
-        if (pedido.startsWith('20000')) {
-            return { flag: 'registro_nao_encontrado_meli', message: 'Nenhum registro encontrado' };
-        }
+        
+        if (pedido.startsWith('20000')) return { flag: 'registro_nao_encontrado_meli', message: 'Nenhum registro encontrado' };
 
         return { flag: 'registro_nao_encontrado', message: 'Nenhum registro encontrado' };
     }
 
-    return await validaParaCancelamentoTroca(result, contactId);
+    return await processaValidacaoTroca(result, contactId);
 };
 
 
-const validaParaCancelamentoTroca = async (result, contactId) => {
+const processaValidacaoTroca = async (result, contactId) => {
     
-    const { marketplace_pedido: pedido, email, razsocial: nome, portal } = result[0];
+    const { pedido, email, razsocial: nome, portal } = result[0];
 
-    let msg = `${nome}, encontramos seu pedido 
-Pedido : *${pedido}* 
-Email : ${email}`;
+    let msg = ` ${nome}, encontramos seu pedido\n Pedido : *${pedido}*\n Email : ${email}`;
+
     await enviaMensagem(msg, contactId);
 
     msg = `Aguarde um momento iremos te transferir para um dos nossos atendentes.`;
     await enviaMensagem(msg, contactId);
 
-    if (portal === 'Mercado Livre' || pedido.startsWith('20000')) {
-        return { flag: 'encaminha_troca_meli', message: 'Encontrado' };
-    }
+    const flag = (portal === 'Mercado Livre' || pedido.startsWith('20000')) 
+    ? 'encaminha_troca_meli' 
+    : 'encaminha_troca_sac';
 
-    return { flag: 'encaminha_troca_sac', message: 'Encontrado' };
+
+    return { flag, message: 'Encontrado' };
 };
 
 
-
-// Envia NFE
-// Envia NFE - MOVIDO PARA O TOPO
 const enviaNFE = async (sequelize, contactId, result) => {
   
     const chaveNfe = result[0].chavenfe;
-    const msg = `Para acessar sua NFE, acesse: 
-https://www.nfe.fazenda.gov.br/portal/consultaRecaptcha.aspx?tipoConsulta=resumo&tipoConteudo=7PhJ+gAVw2g=  
-e insira a seguinte chave :`;
+    const msg = `Para acessar sua NFE, acesse: \n https://www.nfe.fazenda.gov.br/portal/consultaRecaptcha.aspx?tipoConsulta=resumo&tipoConteudo=7PhJ+gAVw2g= \ne insira a seguinte chave :`;
 
     await enviaMensagem(msg, contactId);
     await enviaMensagem(chaveNfe, contactId);
@@ -170,45 +152,28 @@ e insira a seguinte chave :`;
     return { flag: 'nfe_enviada', message: 'Nfe encontrada' };
 };
 
-// Envia NFE pelo pedido
 const enviaNFEPeloPedido = async (pedido, sequelize, contactId) => {
-    const result = await sequelize.query(
-        `SELECT ns.chavenfe
-        FROM nota_saida ns
-        JOIN cliente c ON c.id_cliente = ns.id_cliente
-        WHERE ns.marketplace_pedido = :pedido`,
-        {
-            type: Sequelize.QueryTypes.SELECT,
-            replacements: { pedido }
-        }
-    );
-   
+
+    const result = await pesquisasSql(pedido, 'pedido', sequelize);
 
     if (!result.length) {
-        if (pedido.startsWith('20000')) {
-            return { flag: 'registro_nao_encontrado_meli', message: 'Nenhum registro encontrado' };
-        }
-    
-        return { flag: 'registro_nao_encontrado', message: 'Nenhum registro encontrado' };
+        const flag = pedido.startsWith('20000') 
+            ? 'registro_nao_encontrado_meli' 
+            : 'registro_nao_encontrado';
+        return { flag, message: 'Nenhum registro encontrado' };
     }
-    
 
     return enviaNFE(sequelize, contactId, result);
-
- 
 };
+
 
 // Envia NFE pelo CPF/CNPJ
 const enviaNFEPleoCpf = async (cpf_cnpj, sequelize, contactId) => {
-    const result = await valida(cpf_cnpj, sequelize);
+    const result = await pesquisasSql(cpf_cnpj, 'cpf_cnpj', sequelize)
     
-    if (result === "cpf_invalido") {
-        return { flag: "cpf_invalido", message: "CPF/CNPJ inválido" };
-    }
+    if (result === "cpf_invalido") return { flag: "cpf_invalido", message: "CPF/CNPJ inválido" };
 
-    if (!result.length) {
-        return { flag: 'registro_nao_encontrado', message: 'Nenhum registro encontrado' };
-    }
+    if (!result.length) return { flag: 'registro_nao_encontrado', message: 'Nenhum registro encontrado' };
 
     return enviaNFE(sequelize, contactId, result);
 };
@@ -216,16 +181,13 @@ const enviaNFEPleoCpf = async (cpf_cnpj, sequelize, contactId) => {
 
 
 const validaCpfCnpj = async (cpf_cnpj, sequelize, contactId) => {
-    const result = await valida(cpf_cnpj, sequelize, contactId); // Aguarda a validação
 
-    if (result === "cpf_invalido") {
-        return { flag: "cpf_invalido", message: "CPF/CNPJ inválido" };
-    }
+    const result = await pesquisasSql(cpf_cnpj, 'cpf_cnpj', sequelize)
+
+    if (result === "cpf_invalido") return { flag: "cpf_invalido", message: "CPF/CNPJ inválido" };
     
-    if (!result.length) {
-        return { flag: 'registro_nao_encontrado', message: 'Nenhum registro encontrado' };
-    }
-
+    if (!result.length)  return { flag: 'registro_nao_encontrado', message: 'Nenhum registro encontrado' };
+    
 
     const formattedCpfCnpj = formatCpfCnpj(cpf_cnpj);
     await enviaRastreio(formattedCpfCnpj, sequelize, contactId); // Aguarda o envio do rastreio
@@ -234,58 +196,31 @@ const validaCpfCnpj = async (cpf_cnpj, sequelize, contactId) => {
 };
 
 
-// Envia rastreio
 const enviaRastreio = async (cpf_cnpj, sequelize, contactId) => {
-    const result = await sequelize.query(
-        `SELECT ns.intelipost_order, 
-        CASE 
-            WHEN ns.parceiro = 'FIDCOMERCIOEXTERIOREIRELI' THEN 'Mercado Livre' 
-            WHEN ns.parceiro LIKE '%WAPSTORE%' THEN 'Site Fid Comex' 
-            ELSE ns.parceiro 
-        END AS portal, 
-        ns.marketplace_pedido as pedido
-        FROM nota_saida ns
-        JOIN cliente c ON c.id_cliente = ns.id_cliente
-        WHERE c.cpf = :cpf_cnpj OR c.cnpj = :cpf_cnpj OR ns.marketplace_pedido = :cpf_cnpj
-        LIMIT 1`,
-        {
-            type: Sequelize.QueryTypes.SELECT,
-            replacements: { cpf_cnpj }
-        }
-    );
 
-    if (!result.length || !result[0].intelipost_order) {
+    const result = await pesquisasSql(cpf_cnpj, 'cpf_cnpj', sequelize);
+
+    if (!result?.intelipost_order) {
         return { flag: 'registro_nao_encontrado', message: 'Nenhum código de rastreio encontrado' };
     }
 
-    const { intelipost_order, portal, pedido } = result[0];
+    const { intelipost_order, portal, pedido } = result;
     const rastreioUrl = `https://fidcomex.up.railway.app/rastreio/${intelipost_order}`;
-
     const msg = `Encontramos seu pedido do *${portal}*\nPedido: ${pedido}\n\nO link de rastreio é:\n${rastreioUrl}`;
-    await enviaMensagem(msg, contactId);
 
+    await enviaMensagem(msg, contactId);
     setTimeout(() => enviaMensagem("Para encerrar, digite *fim*", contactId), 1000);
 
-    return { flag: 'rastreio_enviado', message:'Rastreio enviado'};
+    return { flag: 'rastreio_enviado', message: 'Rastreio enviado' };
 };
+
 
 // Valida pedido
 const validaPedido = async (pedido, sequelize, contactId) => {
-    const result = await sequelize.query(
-        `SELECT ns.intelipost_order
-        FROM nota_saida ns
-        JOIN cliente c ON c.id_cliente = ns.id_cliente
-        WHERE ns.marketplace_pedido = :pedido
-        LIMIT 1`,
-        {
-            type: Sequelize.QueryTypes.SELECT,
-            replacements: { pedido }
-        }
-    );
 
-    if (!result.length || !result[0].intelipost_order) {
-        return { flag: 'pedido_nao_encontrado', message: 'Nenhum pedido encontrado' };
-    }
+    const result = await pesquisasSql(pedido, 'pedido', sequelize);
+
+    if (!result.length || !result[0].intelipost_order) return { flag: 'pedido_nao_encontrado', message: 'Nenhum pedido encontrado' };
 
     await enviaMensagem(`Seu pedido foi encontrado! Código: ${result[0].intelipost_order}`, contactId);
 
@@ -297,32 +232,11 @@ const validaPedido = async (pedido, sequelize, contactId) => {
 const validaEmailOutrosAssuntos = async (email, sequelize, contactId) => {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-    if (!email || !emailRegex.test(email)) {
-        return { flag: 'email_invalido', message: 'Email inválido' };
-    }
+    if (!email || !emailRegex.test(email)) return { flag: 'email_invalido', message: 'Email inválido' };
 
-    const result = await sequelize.query(
-        `SELECT ns.chavenfe,
-                ns.marketplace_pedido as pedido,
-                c.email,
-                ns.intelipost_order,
-                CASE 
-                    WHEN ns.parceiro = 'FIDCOMERCIOEXTERIOREIRELI' THEN 'Mercado Livre' 
-                    WHEN ns.parceiro LIKE '%WAPSTORE%' THEN 'Site Fid ComeX' 
-                    ELSE ns.parceiro 
-	            END AS portal
-        FROM nota_saida ns
-        JOIN cliente c ON c.id_cliente = ns.id_cliente
-        WHERE c.email = :email
-        and ns.chavenfe <> ''`,
-        {
-            type: Sequelize.QueryTypes.SELECT,
-            replacements: { email }
-        }
-    );
-
+    const result = await pesquisasSql(email, 'email', sequelize);
+  
     if (result.length ) {
-        console.log(result)
         const { intelipost_order, portal, pedido } = result[0];
         const rastreioUrl = `https://fidcomex.up.railway.app/rastreio/${intelipost_order}`;
 
@@ -336,8 +250,6 @@ const validaEmailOutrosAssuntos = async (email, sequelize, contactId) => {
 
     return { flag: 'email_valido', message: 'Email válido' };
 };
-
-
 
 
 
